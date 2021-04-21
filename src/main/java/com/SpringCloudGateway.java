4d5e7f7c450820390a61b70ddea0f6ca5384ce86
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2021-04-20 19:31:21 -0500

    added route config

--- src/main/java/com/SpringCloudGateway.java
+++ src/main/java/com/SpringCloudGateway.java
@@ -3,16 +3,19 @@ package com;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class SpringCloudGateway {
 
     //TODO create service routes
-//    @Bean
-//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
-//        return builder.routes()
-//                .route(myRoute -> myRoute.path("/api1/**").uri("lb://bookservice"))
-//                .route(myRoute -> myRoute.path("/api2/**").uri("lb://userservice"))
-//                .build();
-//    }
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(myRoute -> myRoute.path("/api/**").uri("lb://tempservice"))
+                .route(myRoute -> myRoute.path("/api2/**").uri("lb://tempservice2"))
+                .build();
+    }
 
 }
